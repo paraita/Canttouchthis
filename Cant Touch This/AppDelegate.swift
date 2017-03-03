ec373e187f3d44b5e88e55113a16710d90f255ac
Paraita Wohler <milkyboi@gmail.com>
2017-03-03 15:42:46 +0100

    Initial Commit

---
+++ Cant Touch This/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Cant Touch This
+//
+//  Created by Paraita Wohler on 03/03/2017.
+//  Copyright © 2017 Paraita. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Cant Touch This/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Cant Touch This
+//
+//  Created by Paraita Wohler on 03/03/2017.
+//  Copyright © 2017 Paraita. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
